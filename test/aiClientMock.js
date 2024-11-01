259b0a1e191e195ea2a7f2e9eb417b149ec1bcc7
Ionut Predoi <ionut.predoi@education.gov.uk>
2024-11-01 11:40:49 +0000

    DSI-7294 - Add functionality to mask email addresses found in requests data

--- test/aiClientMock.js
+++ test/aiClientMock.js
@@ -1,12 +1,12 @@
-module.exports = () => {
-  return {
-    trackEvent: jest.fn(),
-    trackTrace: jest.fn(),
-    trackException: jest.fn(),
-    mockResetAll: function() {
-      this.trackEvent.mockReset();
-      this.trackTrace.mockReset();
-      this.trackException.mockReset();
-    },
-  };
-};
+module.exports = () => ({
+  trackEvent: jest.fn(),
+  addTelemetryProcessor: jest.fn(),
+  trackTrace: jest.fn(),
+  trackException: jest.fn(),
+  mockResetAll() {
+    this.trackEvent.mockReset();
+    this.trackTrace.mockReset();
+    this.trackException.mockReset();
+    this.addTelemetryProcessor.mockReset();
+  },
+});
